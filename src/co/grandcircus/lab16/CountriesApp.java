b5ab0a471aa640be3e7779513ce5daa174c0a428
foleya <andrew@clicktutor.io>
2018-08-01 16:15:36 -0400

    added methods for listing all countries and adding a country

--- src/co/grandcircus/lab16/CountriesApp.java
+++ src/co/grandcircus/lab16/CountriesApp.java
@@ -1,27 +1,82 @@
 package co.grandcircus.lab16;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class CountriesApp {
 
 	public static void main(String[] args) {
-		CountryFileUtil fileUtil = new CountryFileUtil();
-		
-		
-		// Print a list of countries
-		List<Country> list = fileUtil.readFile();
-		for (Country c : list) {
-			System.out.println(c);
-		}
-		
-		// Write a new country
-		Country country = new Country("USA");
-		fileUtil.appendLine(country);
-		
-		// Delete a country??
-		
-		// Quit the app
 
+		CountryFileUtil fileUtil = new CountryFileUtil();
+		Scanner scnr = new Scanner(System.in);
+		
+		boolean programRunning = true;
+		System.out.println("Welcome to the Country List Reader/Writer!");
+
+		do {
+			List<String> mainMenuOptions = new ArrayList<String>(
+					Arrays.asList("List Countries", "Add Country", "Delete Country", "Quit Program"));
+			System.out.println("Main Menu: What would you like to do?");
+			for (int i = 0; i < mainMenuOptions.size(); i++) {
+				System.out.println((i + 1) + ". " + mainMenuOptions.get(i));
+			}
+			boolean menuChoiceIsValid = false;
+			int userInput = 0;
+			do {
+				try {
+					userInput = Integer.parseInt(scnr.nextLine().trim());
+					Validators.validateChoiceInMenu(userInput, mainMenuOptions.size());
+					menuChoiceIsValid = true;
+				} catch (IllegalArgumentException ex) {
+					System.out.println(
+							"You must enter an option between 1 and " + mainMenuOptions.size() + ". Please Try again:");
+				}
+			} while (!menuChoiceIsValid);
+			String userChoice = mainMenuOptions.get(userInput - 1);
+			switch (userChoice) {
+			case ("List Countries"):
+				listCountries(fileUtil);
+				pause(scnr);
+				break;
+			case ("Add Country"):
+				addCountry(fileUtil, scnr);
+				pause(scnr);
+				break;
+			case ("Delete Country"):
+				break;
+			case ("Quit Program"):
+				programRunning = false;
+				break;
+			}
+
+		} while (programRunning);
+
+		System.out.println("\nThanks for using the Country List Reader/Writer! Goodbye!");
+
+	}
+
+	// I just pause the program by forcing the user to hit enter to continue!
+	private static void pause(Scanner scnr) {
+		System.out.println("\nHit enter to return to the Main Menu");
+		scnr.nextLine();
+	}
+
+	private static void addCountry(CountryFileUtil fileUtil, Scanner scnr) {
+		System.out.println("Please enter the name of the country you would like to add to the list:");
+		String countryName = Validators.getValidCountry(scnr);
+		Country country = new Country(countryName);
+		fileUtil.appendLine(country);
+		System.out.println("Great! " + countryName + " was added to the list!");
+	}
+
+	private static void listCountries(CountryFileUtil fileUtil) {
+		System.out.println("Here is the current list of countries:");
+		List<Country> list = fileUtil.readFile();
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println((i + 1) + ". " + list.get(i).name);
+		}
 	}
 
 }
--- src/co/grandcircus/lab16/Validators.java
+++ src/co/grandcircus/lab16/Validators.java
@@ -1,5 +1,33 @@
 package co.grandcircus.lab16;
 
+import java.util.Scanner;
+
 public class Validators {
+
+	public static String getValidCountry(Scanner scnr) {
+		try {
+			String userInput = scnr.nextLine().trim();
+			validateAlphabeticOnly(userInput);
+			return userInput;
+		} catch (IllegalArgumentException ex) {
+			System.out.println(ex.getMessage());
+			return getValidCountry(scnr);
+		}
+	}
+
+	public static void validateAlphabeticOnly(String input) {
+		if (!input.matches("^(?!.*  )[a-zA-Z ]*$")) {
+			throw new IllegalArgumentException(
+					"-- You may only enter alphabetic characters (no numbers, special characters) --");
+		}
+	}
+
+	public static void validateChoiceInMenu(int userInput, int optionsLength) {
+		// This method just validates that a user has selected a number within the
+		// bounds of the menu
+		if (userInput < 1 || userInput > optionsLength) {
+			throw new IllegalArgumentException();
+		}
+	}
 
 }
